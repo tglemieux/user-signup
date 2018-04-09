6b206c6d5837d2117176217863370de1da699d67
Tim Lemieux <tglemieux@gmail.com>
2018-04-08 22:25:37 -0500

    Actually, now I'm done

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from flask import Flask, request, redirect, render_template
 import cgi
-import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 
